f2db24476d6593f391dcc4366fb3e4d1f2b431f8
Mircea Bereveanu <mircea.bereveanu@gmail.com>
2020-04-18 21:24:42 +0300

    optimized start.sh

--- start.sh
+++ start.sh
@@ -3,20 +3,21 @@
 
 cd /usr/src/app/
 if [ ! -d "node_modules" ]; then
+  mkdir /usr/src/app/node_modules &&
+    chmod 777 -R /usr/src/app/node_modules &&
     npm install
 fi
 
 cd /usr/src/app/app/
 if [ ! -d "node_modules" ]; then
+  mkdir /usr/src/app/app/node_modules &&
+    chmod 777 -R /usr/src/app/app/node_modules &&
     npm install
 fi
 
-chmod 777 -R /usr/src/app/node_modules
-chmod 777 -R /usr/src/app/app/node_modules
-
 cd /usr/src/app/app/
 if [ ! -d "dist" ]; then
-    npm run buildP
+  npm run buildP
 fi
 
 cd /usr/src/app/ &&
